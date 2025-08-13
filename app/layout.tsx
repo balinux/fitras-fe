76384830ae9d6ab70b7aa1cc31e9b0d553e8324b
balinux <ryojuniyantara@gmail.com>
2025-08-13 15:42:53 +0800

    feat: authentication setup clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,14 @@
 import type { Metadata } from "next";
+
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -23,12 +33,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+  <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+       
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
