9c0b1838f8fca6a458e41de56a9ccb86e4110eac
balinux <ryojuniyantara@gmail.com>
2025-08-14 13:46:32 +0800

    add: header - add sheed ui

--- components/nav-button.tsx
+++ components/nav-button.tsx
@@ -3,20 +3,25 @@ import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";
 
 interface NavButtonProps {
-href: string;
-label: string;
-isActive: boolean;
+  href: string;
+  label: string;
+  isActive: boolean;
 }
 
-export default function NavButton({href, label, isActive}: NavButtonProps) {
-    return(
-        <Button asChild size="sm" variant="outline" className={cn(
-            "w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition",
-            isActive ? "bg-white/10 text-white" : "bg-transparent",
-        )}>
-            <Link href={href}>
-                <span >{label}</span>
-            </Link>
-        </Button>
-    )
+export default function NavButton({ href, label, isActive }: NavButtonProps) {
+  return (
+    <Button
+      asChild
+      size="sm"
+      variant="outline"
+      className={cn(
+        "w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition",
+        isActive ? "bg-white/10 text-white" : "bg-transparent",
+      )}
+    >
+      <Link href={href}>
+        <span>{label}</span>
+      </Link>
+    </Button>
+  );
 }
