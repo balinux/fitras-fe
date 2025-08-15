790b9b9f9e35a9c25c02387bd5be22f8570c9766
balinux <ryojuniyantara@gmail.com>
2025-08-15 17:38:43 +0800

    add: account setup form

---
+++ features/accounts/components/account-form.tsx
@@ -0,0 +1,102 @@
+import { insertAccountSchema } from "@/db/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormDescription,
+  FormMessage,
+  FormControl,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type formValues = z.infer<typeof formSchema>;
+
+type Props = {
+  id?: string;
+  defaultValue?: formValues;
+  onSubmit: (values: formValues) => void;
+  onDelete?: () => void;
+  disabled?: boolean;
+};
+
+export default function AccountForm({
+  id,
+  defaultValue,
+  onSubmit,
+  onDelete,
+  disabled,
+}: Props) {
+
+  const form = useForm<formValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: defaultValue,
+  });
+
+  const handleSubmit = async (values: formValues) => {
+    onSubmit(values)
+  };
+
+  const handleDelete = () => {
+    onDelete?.();
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="space-x-4 pt-4 mx-5"
+      >
+        <FormField
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input
+                  disabled={disabled}
+                  placeholder="Account name"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          disabled={disabled}
+          className="w-full mt-5"
+          type="submit"
+          onClick={() => form.handleSubmit(handleSubmit)}
+        >
+          {id ? "Save changes" : "Create account"}
+        </Button>
+
+        {!!id && (
+          <Button
+          disabled={disabled}
+          type="button"
+          onClick={handleDelete}
+          variant="destructive"
+          className="w-full mt-5"
+        >
+          <Trash className="size-4 mr-2" />
+          Delete Account
+        </Button>
+      )}
+      </form>
+    </Form>
+  );
+}
--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -11,18 +11,38 @@ import {
   SheetClose,
 } from "@/components/ui/sheet";
 import useNewAccountStore from "@/features/accounts/hooks/use-new-accout-hook";
+import AccountForm from "@/features/accounts/components/account-form";
+import { insertAccountSchema } from "@/db/schema";
+import z from "zod";
 
 export default function NewAccountSheet() {
   const { isOpen, onClose } = useNewAccountStore();
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const formSchema = insertAccountSchema.pick({
+    name: true,
+  });
+
+  type formValues = z.infer<typeof formSchema>;
+
+  const onSubmit = (values: formValues) => {
+    console.log(values);
+  };
+
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
-      <SheetContent side="right">
+      <SheetContent side="right" className="space-y-4">
         <SheetHeader>
           <SheetTitle>New Account</SheetTitle>
           <SheetDescription>
             Create new account to track your expenses
           </SheetDescription>
         </SheetHeader>
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={false}
+          defaultValue={{ name: "" }}
+        />
         <SheetFooter>
           <SheetClose asChild>
             <Button variant="outline">Cancel</Button>
