904494598ab97120824569b7dbf131f99e13f2bc
balinux <ryojuniyantara@gmail.com>
2025-08-19 09:01:41 +0800

    add: datatable - add delete on dropdownitem

--- app/(dashboard)/accounts/actions.tsx
+++ app/(dashboard)/accounts/actions.tsx
@@ -2,8 +2,10 @@
 
 import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-acount";
 import useEditAccountStore from "@/features/accounts/hooks/use-edit-account-hook";
-import { Edit, MoreHorizontal } from "lucide-react";
+import { useConfirm } from "@/hooks/use-confirm";
+import { Edit, MoreHorizontal, Trash2 } from "lucide-react";
 
 interface ActionProps {
     id: string;
@@ -13,8 +15,26 @@ export default function Actions({ id }: ActionProps) {
     // use edit account hook and pass id to it
     const { onOpen } = useEditAccountStore();
 
+    // use delete account hook and pass id to it
+    const { mutate: deleteMutate } = useDeleteAccount(id);
+
+    // confirmation hooks
+    const [ConfirmDelete, confirm] = useConfirm(
+        "are you sure you want to delete this account?",
+        "Delete Account",
+    )
+
+    // on delete function
+    const onDelete = async () => {
+        const ok = await confirm();
+        if (ok) {
+            deleteMutate();
+        }
+    }
+
     return (
         <>
+        <ConfirmDelete/>
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                     <Button variant='ghost' className="size-8 p-0">
@@ -30,6 +50,15 @@ export default function Actions({ id }: ActionProps) {
 
                         <Edit className="size-4 mr-2" />
                         Edit
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                        variant="destructive"
+                        disabled={false}
+                        onClick={onDelete}
+                        className="flex gap-2"
+                    >
+                        <Trash2 className="size-4 mr-2" />
+                        Delete
                     </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
