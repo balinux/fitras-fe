2960e0a85513cef9cc5d1c4c34ff3cd7b1f2c274
balinux <ryojuniyantara@gmail.com>
2025-09-02 18:15:50 +0800

    feat: summary -  add api for summary

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
+import { eachDayOfInterval, isSameDay } from "date-fns";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -19,4 +20,52 @@ export function formatCurrency(amount: number) {
     currency: "USD",
     minimumFractionDigits: 2,
   }).format(amount);
+}
+
+export function calculatePercentageChange(
+  current: number,
+  previous: number
+) {
+  if (previous == 0) {
+    return previous === current ? 0 : 100
+  }
+
+  return (current - previous) / previous * 100;
+}
+
+export function fillMissingDays(
+  activeDays: {
+    date: Date,
+    income: number,
+    expenses: number
+  }[],
+  startDate: Date,
+  endDate: Date
+) {
+
+  // check leng of active day
+  if (activeDays.length == 0) {
+    return []
+  }
+  const allDays = eachDayOfInterval({
+    start: startDate,
+    end: endDate
+  })
+
+  // fill all transastion by days
+  const transactionByDay = allDays.map((day) => {
+    const activeDay = activeDays.find((activeDay) => isSameDay(activeDay.date, day))
+
+    if (activeDay) {
+      return activeDay;
+    } else {
+      return {
+        date: day,
+        income: 0,
+        expenses: 0
+      }
+    }
+  })
+
+  return transactionByDay;
 }
