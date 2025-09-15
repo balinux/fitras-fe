6f69cd05c1be89c194a931a12d3ea63d13154a92
balinux <ryojuniyantara@gmail.com>
2025-09-16 07:04:17 +0800

    fotmat linter

--- components/filters.tsx
+++ components/filters.tsx
@@ -1,9 +1,9 @@
 import AccountFilter from "./account-filter";
 
 export default function Filters() {
-    return (
-        <div className="flex flex-col lg:flex-row items-center gap-y-2 lg:gap-y-0 lg:gap-x-2">
-            <AccountFilter/>
-        </div>
-    )
+  return (
+    <div className="flex flex-col lg:flex-row items-center gap-y-2 lg:gap-y-0 lg:gap-x-2">
+      <AccountFilter />
+    </div>
+  );
 }
